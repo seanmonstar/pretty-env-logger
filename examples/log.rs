57a94aaf43adb039913a75eee0a0dd4c100f7689
Sean McArthur <sean.monstar@gmail.com>
2016-10-19 18:46:39 -0700

    init

---
+++ examples/log.rs
@@ -0,0 +1,12 @@
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
+fn main() {
+    pretty_env_logger::init();
+
+    trace!("a trace example");
+    debug!("deboogging");
+    info!("such information");
+    warn!("o_O");
+    error!("boom");
+}
---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+extern crate ansi_term;
+extern crate env_logger;
+extern crate log;
+
+use std::fmt;
+use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+
+use ansi_term::{Color, Style};
+use env_logger::LogBuilder;
+use log::LogLevel;
+
+struct Level(LogLevel);
+
+impl fmt::Display for Level {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.0 {
+            LogLevel::Trace => Color::Purple.paint("TRACE"),
+            LogLevel::Debug => Color::Blue.paint("DEBUG"),
+            LogLevel::Info => Color::Green.paint("INFO "),
+            LogLevel::Warn => Color::Yellow.paint("WARN "),
+            LogLevel::Error => Color::Red.paint("ERROR")
+        }.fmt(f)
+    }
+}
+
+static MAX_MODULE_WIDTH: AtomicUsize = ATOMIC_USIZE_INIT;
+
+pub fn init() {
+    let mut builder = LogBuilder::new();
+
+    builder.format(|record| {
+        let mut module_path = record.location().module_path().to_string();
+        let max_width = MAX_MODULE_WIDTH.load(Ordering::Relaxed);
+        if max_width > module_path.len() {
+            let diff = max_width - module_path.len();
+            module_path.extend(::std::iter::repeat(' ').take(diff));
+        } else {
+            MAX_MODULE_WIDTH.store(module_path.len(), Ordering::Relaxed);
+        }
+        format!("{}:{}: {}",
+                Level(record.level()),
+                Style::new().bold().paint(module_path),
+                record.args())
+    });
+
+    if let Ok(s) = ::std::env::var("RUST_LOG") {
+        builder.parse(&s);
+    }
+
+    builder.init().unwrap();
+}
