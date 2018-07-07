57963895dac8a7c800dd5c46cefa9b14f5b4301a
avi.srivastava <asrivastava@cs.stonybrook.edu>
2018-07-07 12:24:58 -0700

    crono patch

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,12 @@
 extern crate ansi_term;
 extern crate env_logger;
 extern crate log;
+extern crate chrono;
 
 use std::fmt;
 use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 
+use chrono::Local;
 use ansi_term::{Color, Style};
 use env_logger::Builder;
 use log::Level;
@@ -62,6 +64,20 @@ pub fn init() {
     try_init().unwrap();
 }
 
+/// Initializes the global logger with a timed pretty env logger.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Panics
+///
+/// This function fails to set the global logger if one has already been set.
+#[inline]
+pub fn init_timed() {
+    try_init_timed().unwrap();
+}
+
 /// Initializes the global logger with a pretty env logger.
 ///
 /// This should be called early in the execution of a Rust program, and the
@@ -73,6 +89,19 @@ pub fn init() {
 /// This function fails to set the global logger if one has already been set.
 pub fn try_init() -> Result<(), log::SetLoggerError> {
     try_init_custom_env("RUST_LOG")
+}
+
+/// Initializes the global logger with a timed pretty env logger.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Errors
+///
+/// This function fails to set the global logger if one has already been set.
+pub fn try_init_timed() -> Result<(), log::SetLoggerError> {
+    try_init_timed_custom_env("RUST_LOG")
 }
 
 /// Initialized the global logger with a pretty env logger, with a custom variable name.
@@ -107,6 +136,25 @@ pub fn try_init_custom_env(environment_variable_name: &str) -> Result<(), log::S
     builder.try_init()
 }
 
+/// Initialized the global logger with a timed pretty env logger, with a custom variable name.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Errors
+///
+/// This function fails to set the global logger if one has already been set.
+pub fn try_init_timed_custom_env(environment_variable_name: &str) -> Result<(), log::SetLoggerError> {
+    let mut builder = formatted_timed_builder()?;
+
+    if let Ok(s) = ::std::env::var(environment_variable_name) {
+        builder.parse(&s);
+    }
+
+    builder.try_init()
+}
+
 /// Returns a `env_logger::Builder` for further customization.
 ///
 /// This method will return a colored and formatted) `env_logger::Builder`
@@ -133,6 +181,40 @@ pub fn formatted_builder() -> Result<Builder, log::SetLoggerError> {
         }
         writeln!(f, " {} {} > {}",
                  ColorLevel(record.level()),
+                 Style::new().bold().paint(format!("{: <width$}", target, width=max_width)),
+                 record.args())
+    });
+
+    Ok(builder)
+}
+
+/// Returns a `env_logger::Builder` for further customization.
+///
+/// This method will return a colored and time formatted) `env_logger::Builder`
+/// for further customization. Tefer to env_logger::Build crate documentation
+/// for further details and usage.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Errors
+///
+/// This function fails to set the global logger if one has already been set.
+pub fn formatted_timed_builder() -> Result<Builder, log::SetLoggerError> {
+    let mut builder = Builder::new();
+
+    builder.format(|f, record| {
+        use std::io::Write;
+        let target = record.target();
+        let mut max_width = MAX_MODULE_WIDTH.load(Ordering::Relaxed);
+        if max_width < target.len() {
+            MAX_MODULE_WIDTH.store(target.len(), Ordering::Relaxed);
+            max_width = target.len();
+        }
+        writeln!(f, " {}:[{}] {} > {}",
+                 ColorLevel(record.level()),
+                 Local::now().format("%Y-%m-%dT%H:%M:%S"),
                  Style::new().bold().paint(format!("{: <width$}", target, width=max_width)),
                  record.args())
     });
