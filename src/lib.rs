16414224598d23cec9fe68215004fd165299dd7d
Ibraheem Ahmed <ibrah1440@gmail.com>
2021-02-21 13:36:41 -0500

    update lib.rs example to rust 2018 edition

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,14 @@
 //! ## Example
 //!
 //! ```
-//! extern crate pretty_env_logger;
-//! #[macro_use] extern crate log;
-//!
 //! fn main() {
 //!     pretty_env_logger::init();
 //!
-//!     trace!("a trace example");
-//!     debug!("deboogging");
-//!     info!("such information");
-//!     warn!("o_O");
-//!     error!("boom");
+//!     log::trace!("a trace example");
+//!     log::debug!("deboogging");
+//!     log::info!("so much information");
+//!     log::warn!("o_O");
+//!     log::error!("boom");
 //! }
 //! ```
 //!
